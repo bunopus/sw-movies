088d87a4f38c8272cc33b8fe7222422555d59155
Evgeny Gusev <bunopus@gmail.com>
2018-05-22 02:10:53 +0300

    2. Mock database, and define Movie model (thanks https://github.com/Package/Star-Wars-Express for json)

---
+++ server/lib/src/movie.dart
@@ -0,0 +1,13 @@
+
+class Movie {
+  String title;
+  int episodeNumber;
+
+  List<String> mainCharacters;
+  String description;
+  String poster;
+  String heroImage;
+
+  Movie(this.title, this.episodeNumber, this.mainCharacters,
+      this.description, this.poster, this.heroImage);
+}
