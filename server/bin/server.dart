f5048497a4417c4c361cfe3edd299dbb472802ba
Evgeny Kot <bunopus@gmail.com>
2018-11-27 11:47:05 +0300

    1. Start with simple server without any additional dependencies. Just listen on the localhost for the requests.

---
+++ server/bin/server.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'dart:io';
+
+Future main() async {
+  const port = 8000;
+
+  final server = await HttpServer.bind(
+    InternetAddress.loopbackIPv4,
+    port,
+  );
+
+  print('Listening on ${port}');
+
+  await for (HttpRequest request in server) {
+    request.response
+      ..write('Hello, world!')
+      ..close();
+  }
+}
+
